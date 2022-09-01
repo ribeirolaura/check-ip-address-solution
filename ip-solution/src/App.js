210aa23e851fb2d47a81bf66f862ff7b6cf4b815
Ribeiro <lauradeor@gmail.com>
2022-09-01 19:43:47 -0300

    feat: get the IP number using IPIFY.org

--- ip-solution/src/App.js
+++ ip-solution/src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
+import "./App.css";
+import {useState} from 'react';
 
 function App() {
+
+  const [IPNumber, setIPNumber] = useState(); 
+
+  function getIPAddress() {
+    fetch('https://api.ipify.org/?format=json')
+      .then(res => res.json())
+      .then(data => setIPNumber(data.ip))
+  }
+
+
+
   return (
     <div className="App">
       <h1>Check IP Solution</h1>
-      <button className="main-btn">Check your IP</button> 
-      <span>Your IP Address is: </span> 
-      <span>default</span> 
+      <button onClick={getIPAddress} className="main-btn">Check your IP</button>
+      <span>Your IP Address is: </span>
+      <span>{IPNumber}</span>
     </div>
   );
 }
