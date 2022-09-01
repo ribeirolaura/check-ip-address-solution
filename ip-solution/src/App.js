80b69154dcf84af397650465d9ffd0081b1226a9
Ribeiro <lauradeor@gmail.com>
2022-09-01 18:32:53 -0300

    feat: add style and main project structure

--- ip-solution/src/App.js
+++ ip-solution/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Check IP Solution</h1>
+      <button className="main-btn">Check your IP</button> 
+      <span>Your IP Address is: </span> 
+      <span>default</span> 
     </div>
   );
 }
